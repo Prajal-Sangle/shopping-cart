32964bb3ffe01c1db98e5e2fca95303a62b086a6
Prajal Sangle <prajalsangle@gmail.com>
2023-03-26 12:12:19 +0530

    first commit

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "Jasmine",
+    price: 30.00,
+    instock: 50,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t1.png",
+  },
+  {
+    id: 1,
+    name: "Rose",
+    price: 25.00,
+    instock: 22,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t2.png",
+  },
+  {
+    id: 2,
+    name: "Marigold",
+    price: 20.00,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t3.png",
+  },
+  {
+    id: 3,
+    name: "Orchids",
+    price: 30.00,
+    instock: 5,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t4.png",
+  },
+  {
+    id: 4,
+    name: "Tulips",
+    price: 22.00,
+    instock: 4,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t5.png",
+  },
+  {
+    id: 5,
+    name: "Aster",
+    price: 40.00,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t6.png",
+  },
+];
